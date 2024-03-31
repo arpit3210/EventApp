36202af9b92cbf2d8a092dbb55faee73e1effb64
1234 <arpitsinghthakur321@gmail.com>
2024-03-31 11:11:45 +0530

    changes authmiddleware path

--- my-project/middleware.ts
+++ my-project/middleware.ts
@@ -1,6 +1,5 @@
-// import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
 
-import { authMiddleware } from "@clerk/nextjs/server";
 
  
 export default authMiddleware({
